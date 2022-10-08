2bf397919d8e788d5037518f7b5f4f6d3844ceb2
Juliette Bernheisel <eu@Juliettes-MacBook-Pro.local>
2022-10-08 08:54:39 -0400

    Attempt to fix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,38 @@
 {
-    description = "Juliette's Cool Flake";
 
-    outputs = { self, nixpkgs }: {
-        packages.x86_64-darwin.default = with import nixpkgs { system = "x86_64-darwin"; };
-        stdenv.mkDerivation {
-            name = "batteryBuddy";
-            src = fetchzip {
-                url = "https://batterybuddy.app/releases/Battery%20Buddy.zip";
-                sha256 = "D52mdxHcqQ4rs8GuQwcNt69wjWFZz+XH3MqfS7Uugxg=";
-            };
-            installPhase = ''
-            runHook preInstall
-            APP_DIR="$out/Applications/battery buddy.app"
-            mkdir -p "$APP_DIR"
-            cp -r . "$APP_DIR"
-            runHook postInstall
-            '';
+    inputs = {
+        nixpkgs = {
+            url = "github:nixos/nixpkgs/nixos-unstable";
+        };
+        flake-utils = {
+            url = "github:numtide/flake-utils";
         };
     };
+    outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+            let
+            pkgs = import nixpkgs {
+            inherit system;
+            };
+            batteryBuddy = (with pkgs; stdenv.mkDerivation {
+                name = "batteryBuddy";
+                src = fetchzip {
+                url = "https://batterybuddy.app/releases/Battery%20Buddy.zip";
+                sha256 = "D52mdxHcqQ4rs8GuQwcNt69wjWFZz+XH3MqfS7Uugxg=";
+                };
+                installPhase = ''
+                runHook preInstall
+                APP_DIR="$out/Applications/battery buddy.app"
+                mkdir -p "$APP_DIR"
+                cp -r . "$APP_DIR"
+                runHook postInstall
+                '';
+                });
+            in rec {
+#            defaultApp = flake-utils.lib.mkApp {
+#                drv = defaultPackage;
+#            };
+            defaultPackage = batteryBuddy;
+
+            }
+            );
 }
