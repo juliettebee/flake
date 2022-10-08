e21ab57fa12fac00f47a3464c2889b9e42ba33a8
Juliette Bernheisel <eu@Juliettes-MacBook-Pro.local>
2022-10-08 00:33:26 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,21 @@
 {
-  description = "Juliette's Cool Flake";
+    description = "Juliette's Cool Flake";
 
-  outputs = { self, nixpkgs }: {
-    batteryBuddy.url = "https://batterybuddy.app/releases/Battery%20Buddy.zip";
-    batteryBuddy.flake = false;
-  };
+    outputs = { self, nixpkgs }: {
+        packages.x86_64-darwin.default = with import nixpkgs { system = "x86_64-darwin"; };
+        stdenv.mkDerivation {
+            name = "batteryBuddy";
+            src = fetchzip {
+                url = "https://batterybuddy.app/releases/Battery%20Buddy.zip";
+                sha256 = "D52mdxHcqQ4rs8GuQwcNt69wjWFZz+XH3MqfS7Uugxg=";
+            };
+            installPhase = ''
+            runHook preInstall
+            APP_DIR="$out/Applications/battery buddy.app"
+            mkdir -p "$APP_DIR"
+            cp -r . "$APP_DIR"
+            runHook postInstall
+            '';
+        };
+    };
 }
